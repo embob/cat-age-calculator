8c285fe5edf0dfb37a8f94054939d3dbdddced98
Emma Karayiannis <emma.karayiannis@gmail.com>
2021-08-14 20:53:09 +0100

    Refactor calculateHumanMonths

--- cat-calculator.js
+++ cat-calculator.js
@@ -65,38 +65,18 @@ function calculateHumanMonths(age) {
   const { humanBefore, humanAfter, catBefore, catAfter } =
     findValuesBeforeAndAfter(age);
 
-  const catYearsInAHumanYear = 4;
-  const oneHumanYearInMonths = 12;
+  const catYearsPerHumanYear = 4;
+  const twelveMonths = 12;
+  const ageMinusCatBefore = age - catBefore;
 
   if (!humanAfter && !catAfter) {
-    const catYearsSinceBefore = (age - catBefore) / catYearsInAHumanYear;
-    const humanYearsSinceBefore = catYearsSinceBefore * oneHumanYearInMonths;
-    return humanBefore + humanYearsSinceBefore;
+    const numberOfCatYears = ageMinusCatBefore / catYearsPerHumanYear;
+    const humanMonthsSinceBefore = numberOfCatYears * twelveMonths;
+    return humanBefore + humanMonthsSinceBefore;
   }
 
-  const humanYearsDifference = humanAfter - humanBefore;
-  const catYearsDifference = catAfter - catBefore;
-  const catYearsFromAgeToBefore = age - catBefore;
-  const humanMonthsFromAgeToBefore = (humanYearsDifference / catYearsDifference) * catYearsFromAgeToBefore;
-  return humanBefore + humanMonthsFromAgeToBefore;
-
-  // const catDifference = catAfter - catBefore;
-  // const humanDifference = humanAfter - humanBefore;
-  // const ratio = humanDifference / catDifference;
-
-  // const a = age - catBefore;
-  // const b = ratio * a;
-  // return humanBefore + b;
-
-// diff between human years   10 - 4 = 6
-// diff between cat years   6 - 3 = 3
-//  ratio 3/6
-// take human year want to seek and minus lower year value from it so 6 years - 4years = 2
-               // take human year want to seek and minus it from higher year value from it so 10 years - 6years = 4
-// for lower no calc: multiple 3/6 * 2 and add to lower no. of cat value
-// for higher no calc: multiply 3/6 * 4 and minus from higher no of cat value.
-
-  // do calc with all 4 values.
+  const humanMonthsSinceBefore = ((humanAfter - humanBefore) / (catAfter - catBefore)) * ageMinusCatBefore;
+  return humanBefore + humanMonthsSinceBefore;
 }
 
 module.exports = {
@@ -105,19 +85,3 @@ module.exports = {
   formatAgeString,
   findValuesBeforeAndAfter,
 };
-
-// Logic notes
-// diff between human years   10 - 4 = 6
-// diff between cat years   6 - 3 = 3
-//  ratio 3/6
-// take human year want to seek and minus lower year value from it so 6 years - 4years = 2
-// take human year want to seek and minus it from higher year value from it so 10 years - 6years = 4
-// for lower no calc: multiple 3/6 * 2 and add to lower no. of cat value
-// for higher no calc: multiply 3/6 * 4 and minus from higher no of cat value.
-
-// for upwards 28
-
-// age inputted - 28; 33 - 28 = 5
-// we know cats age by 4 years every time so going to divide age difference by 4; 5/4 = 1.25
-// cats go up by 12 so multiply 12 by 1.25 = 15
-// add 15 to cats before = 51 months
